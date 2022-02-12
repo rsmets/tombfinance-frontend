3bcc1f98b1369262343e61022dcc176e43ca2e44
Ray Smets <rayjsmets@gmail.com>
2022-02-11 21:29:47 -0700

    [Config] removed local def as local deployment json is part of git ignore.

--- src/config.ts
+++ src/config.ts
@@ -4,29 +4,29 @@ import { Configuration } from './tomb-finance/config';
 import { BankInfo } from './tomb-finance';
 
 const configurations: { [env: string]: Configuration } = {
-  local: {
-    chainId: 1337,
-    networkName: 'Local Hardhat Node',
-    ftmscanUrl: 'https://testnet.ftmscan.com',
-    defaultProvider: 'http://127.0.0.1:8545/', // TODO: figure out how to host a local RPC
-    deployments: require('./tomb-finance/deployments/deployments.local.json'),
-    externalTokens: {
-      WFTM: ['0xf1277d1ed8ad466beddf92ef448a132661956621', 18],
-      FUSDT: ['0xb7f24e6e708eabfaa9e64b40ee21a5adbffb51d6', 6],
-      BOO: ['0x14f0C98e6763a5E13be5CE014d36c2b69cD94a1e', 18],
-      ZOO: ['0x2317610e609674e53D9039aaB85D8cAd8485A7c5', 0],
-      SHIBA: ['0x39523112753956d19A3d6a30E758bd9FF7a8F3C0', 9],
-      'USDT-FTM-LP': ['0xE7e3461C2C03c18301F66Abc9dA1F385f45047bA', 18],
-      'VINYL-FTM-LP': ['0x13Fe199F19c8F719652985488F150762A5E9c3A8', 18],
-      'TRACKS-FTM-LP': ['0x20bc90bB41228cb9ab412036F80CE4Ef0cAf1BD5', 18],
-      'TOMB-FTM-LP': ['0x13Fe199F19c8F719652985488F150762A5E9c3A8', 18],
-      'TSHARE-FTM-LP': ['0x20bc90bB41228cb9ab412036F80CE4Ef0cAf1BD5', 18],
-    },
-    baseLaunchDate: new Date('2022-02-02 13:00:00Z'),
-    bondLaunchesAt: new Date('2022-02-03T15:00:00Z'),
-    masonryLaunchesAt: new Date('2022-02-03T00:00:00Z'),
-    refreshInterval: 10000,
-  },
+  // local: {
+  //   chainId: 1337,
+  //   networkName: 'Local Hardhat Node',
+  //   ftmscanUrl: 'https://testnet.ftmscan.com',
+  //   defaultProvider: 'http://127.0.0.1:8545/', // TODO: figure out how to host a local RPC
+  //   deployments: require('./tomb-finance/deployments/deployments.local.json'),
+  //   externalTokens: {
+  //     WFTM: ['0xf1277d1ed8ad466beddf92ef448a132661956621', 18],
+  //     FUSDT: ['0xb7f24e6e708eabfaa9e64b40ee21a5adbffb51d6', 6],
+  //     BOO: ['0x14f0C98e6763a5E13be5CE014d36c2b69cD94a1e', 18],
+  //     ZOO: ['0x2317610e609674e53D9039aaB85D8cAd8485A7c5', 0],
+  //     SHIBA: ['0x39523112753956d19A3d6a30E758bd9FF7a8F3C0', 9],
+  //     'USDT-FTM-LP': ['0xE7e3461C2C03c18301F66Abc9dA1F385f45047bA', 18],
+  //     'VINYL-FTM-LP': ['0x13Fe199F19c8F719652985488F150762A5E9c3A8', 18],
+  //     'TRACKS-FTM-LP': ['0x20bc90bB41228cb9ab412036F80CE4Ef0cAf1BD5', 18],
+  //     'TOMB-FTM-LP': ['0x13Fe199F19c8F719652985488F150762A5E9c3A8', 18],
+  //     'TSHARE-FTM-LP': ['0x20bc90bB41228cb9ab412036F80CE4Ef0cAf1BD5', 18],
+  //   },
+  //   baseLaunchDate: new Date('2022-02-02 13:00:00Z'),
+  //   bondLaunchesAt: new Date('2022-02-03T15:00:00Z'),
+  //   masonryLaunchesAt: new Date('2022-02-03T00:00:00Z'),
+  //   refreshInterval: 10000,
+  // },
   development: {
     chainId: ChainId.FTMTESTNET,
     networkName: 'Fantom Opera Testnet',
